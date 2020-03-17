d12d5ad7bf3729a8dfc654489dd3e5d366f62ee3
Gabriel <gabriel@faci.ly>
2020-03-17 01:18:30 -0300

    requisições, appBar e loading

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async{
+    http.Response response;
+
+    if(_search == null){
+      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=1dwwjEGDmogbT2Tm292aa9MaCL69RmTr&limit=20&rating=G");
+    }else{
+      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=1dwwjEGDmogbT2Tm292aa9MaCL69RmTr&q=$_search&limit=25&offset=$_offset&rating=G&lang=en");
+    }
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getGifs().then((map){
+        print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child:TextField(
+            decoration: InputDecoration(
+                labelText: "Pesquise aqui",
+                labelStyle: TextStyle(color: Colors.white),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)
+                 ),
+            ),
+            style: TextStyle(color:Colors.white, fontSize: 18.0),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: FutureBuilder(
+            future: _getGifs(),
+            builder: (context, snapshot){
+                switch(snapshot.connectionState){
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+
+                      ),
+                    );
+                    default:
+                      if(snapshot.hasError)return Container();
+                      else _createGifTable(context, snapshot);
+                }
+            }
+          ),
+        )
+        ],
+      ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
+
+  }
+}
